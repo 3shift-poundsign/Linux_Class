00288c2d45e7cb6d0d92e7c90f9bcc32cfe42dd1
ZHJ0125 <shandonghoujin@163.com>
2019-09-27 15:45:06 +0800

    Setember_17th

---
+++ lesson4/test.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+void sort(int *a,int number)
+{
+	int temp;
+	int i,j;
+	for (i=0;i<number-1;i++)
+	{
+		for(j=0;j<number-1-i;j++)
+		{
+			if(*(a+j) < *(a+j+1))
+			{
+				temp = *(a+j);
+				*(a+j) = *(a+j+1);
+				*(a+j+1) = temp;
+			}
+		}
+	}
+}
+
+void show(int *data, int num)
+{
+	int i;
+	for(i=0;i<num;i++)
+	{
+		printf("%d ",data[i]);
+	}
+	printf("\n");
+}
+
+
+void main()
+{
+	int i;
+	int a[5] = {12,34,76,66,98};
+	int *p;
+	p = a;
+	show(a,5);
+	sort(p,5);
+	show(a,5);
+}
---
+++ lesson4/test2.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+
+int a[5] = {78,12,34,64,59}; 
+
+void show(int *data, int num)
+{
+    int i;
+    for(i=0;i<num;i++)
+    {
+        printf("%d ",data[i]);
+    }
+    printf("\n");
+}
+
+
+void function1(int *a,int num1,bool sort)
+{
+    int temp;
+    int i,j;
+    for (i=0;i<num1-1;i++)
+    {   
+        for(j=0;j<num1-1-i;j++)
+        {
+			if(sort == true)
+			{
+            	if(*(a+j) < *(a+j+1))
+            	{
+                	temp = *(a+j);
+                	*(a+j) = *(a+j+1);
+                	*(a+j+1) = temp;
+            	}
+			}
+			if(sort == false)
+            {   
+                if(*(a+j) > *(a+j+1))
+                {   
+                    temp = *(a+j);
+                    *(a+j) = *(a+j+1);
+                    *(a+j+1) = temp;
+                }
+			}
+        }
+    }   
+}
+
+
+void function2()
+{
+	functinon1(a,5,true);
+	show(a,5);
+}
+
+
+void main()
+{
+	function1(a,5,false);
+	show(a,5);
+	function2();
+}
