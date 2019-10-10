d288fdf32e97161d990efb59b09c5d9c8bb1bc9b
ZHJ0125 <shandonghoujin@163.com>
2019-10-10 11:46:00 +0800

    Add lesson7

---
+++ lesson7/test_for3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	for(int i=0 ;; i++)
+	{
+		if(i>=10) break;
+		else printf("HelloWorld!\n");
+	}
+	return 0;
+}
---
+++ lesson7/test_for4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int i=1;
+	int sum=0;
+	loop:
+		if(i<=100)
+		{
+			sum += i;
+			i++;
+			goto loop;
+		}
+	printf("The sum of 100 is %d\n",sum);
+	return 0;
+}
