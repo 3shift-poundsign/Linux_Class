f8f1c0c9e22f84c665a0927d1d41f3cd95d0a3f1
ZHJ0125 <shandonghoujin@163.com>
2019-10-24 10:31:43 +0800

    Add lesson9

---
+++ lesson9/test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define NUM 10
+int number[NUM] = {23,54,34,65,10,34,45,77,24,22};
+
+int* sort(int *num, int len)
+{
+	int i,j;
+	int temp;
+	for(i=0;i<len-1;i++)
+	{
+		for(j=0;j<len-i-1;j++)
+		{
+			if(*(num+j) > *(num+j+1))
+			{
+				temp = *(num+j);
+				*(num+j) = *(num+j+1);
+				*(num+j+1) = temp;
+			}
+		}
+	}
+	return num;
+}
+
+void show(int *sorted, int total)
+{
+	int i,j;
+	for(i=0;i<total;i++)
+	{
+		if(i%5 == 0) printf("\n");
+		printf("%d ",*(sorted+i));
+	}
+	printf("\n\n");
+}
+
+
+int main()
+{
+	show(sort(number,NUM),NUM);
+	return 0;
+}
