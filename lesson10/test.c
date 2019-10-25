1e645e510ded1b58de3b42269fa0747ec30f4b56
ZHJ0125 <shandonghoujin@163.com>
2019-10-25 22:23:21 +0800

    Add Total function

--- lesson10/test.c
+++ lesson10/test.c
@@ -1,16 +1,27 @@
-#include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-#define NUM_OF_STUDENT 10
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define NUM_OF_STUDENT 3
 #define NUM_OF_MARK 6
 
 struct student {
 	char name[10];
 	int mark[NUM_OF_MARK];
+	int sutid;
 	int avg;
 	int total;
 };
 
+
+/*************************************************
+Function: SetMark
+Description: Set struct student's data of marks
+Input: Structure pointer of student *test
+Output: None
+Return: None
+Author: ZhangH.J.
+Date: 2019/10/25
+*************************************************/
 void setmark(struct student* test)
 {
 	int mark;
@@ -18,7 +29,7 @@ void setmark(struct student* test)
 	int less_than60 = 0;
 	int more_than90 = 0;
 	srand(time(NULL));
-	while(con < NUM_OF_STUDENT)	// loop for erery struct
+	while(con < NUM_OF_STUDENT)		// loop for erery struct
 	{
 		int current;
 		for(current=0; current<NUM_OF_MARK;)
@@ -52,13 +63,40 @@ void setmark(struct student* test)
 }
 
 
+
+void Count_Total_Score(struct student* test)
+{
+	int total;
+	int con = 0;
+	while(con < NUM_OF_STUDENT)
+	{
+		int current;
+		for(current=0; current<NUM_OF_MARK; current++)
+		{
+			total += *(((test+(con))->mark)+(current));
+		}
+		((test+(con++))->total) = total;		// Record data into Struct
+	}
+}
+
+
+
+/*************************************************
+Function: ShowMark
+Description: Display struct student's data of marks
+Input: Structure pointer of student *test
+Output: None
+Return: None
+Author: ZhangH.J.
+Date: 2019/10/25
+*************************************************/
 void showmark(struct student* test)
 {
 	int con=0;
 	while(con < NUM_OF_STUDENT)	// loop for every struct
 	{
 		//printf("mark of student %d is %d \n", con, *(test->mark+(con++)));
-		printf("The marks of student%d is :", con);
+		printf("The marks of student %d is :", con);
 		int current = 0;
 		for(current=0;current<NUM_OF_MARK;current++)	// loop for every student 
 		{
@@ -70,11 +108,29 @@ void showmark(struct student* test)
 }
 
 
+
+void Show_Total_Mark(struct student* test)
+{
+	int con = 0;
+	while(con < NUM_OF_STUDENT)
+	{
+		printf("The total mark of student %d is : %d\n",con,((test+(con))->total));
+		con ++;		// loop for next student
+	}
+}
+
+
 int main()
 {
-	struct student stu[NUM_OF_STUDENT];
-	setmark(stu);
-	showmark(stu);
+	struct student stu[NUM_OF_STUDENT];		// define struct student
+	setmark(stu);							// set student's marks
+	showmark(stu);							// display student's marks
+	Count_Total_Score(stu);					// Calculate total marks of every students
+	Show_Total_Mark(stu);					// Display total marks
 
 	return 0;
 }
+
+
+
+
