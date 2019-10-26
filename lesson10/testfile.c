2ae29d4fd8581ce4ff303576e431a947b3ec299f
ZHJ0125 <shandonghoujin@163.com>
2019-10-27 07:59:21 +0800

    Add test file

---
+++ lesson10/testfile.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE *fp;
+	if((fp=fopen("hello","a+"))==NULL)
+	{
+		printf("File cannot be opened\n");
+		exit(0);
+	}
+	else
+	{
+		printf("File opened for writing\n");
+		fprintf(fp, "This is testing for fprintf...\n");
+		fputs("This is testing for fputs...\n", fp);
+	}
+	fclose(fp); 
+
+	return 0;
+}
